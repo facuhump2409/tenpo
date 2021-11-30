1b9caa28db1eff870a5237687276bbdbcc1ad86c
facuhump2409 <facu2409@hotmail.com>
2021-11-30 17:37:22 -0300

    Initial spring structure

---
+++ src/main/kotlin/com/example/tenpo/TenpoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tenpo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TenpoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TenpoApplication>(*args)
+}

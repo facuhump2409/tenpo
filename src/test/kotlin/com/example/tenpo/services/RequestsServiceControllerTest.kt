aef4d83e3a3cad732c278a9136fbdcf056a2d471
facuhump2409 <facu2409@hotmail.com>
2021-12-03 18:48:13 -0300

    Adding a test and fixing docker compose

--- src/test/kotlin/com/example/tenpo/services/RequestsServiceControllerTest.kt
+++ src/test/kotlin/com/example/tenpo/services/RequestsServiceControllerTest.kt
@@ -1,5 +1,6 @@
 package com.example.tenpo.services
 
+import com.example.tenpo.exceptions.CustomException
 import com.example.tenpo.model.requests.Request
 import com.example.tenpo.repos.RequestsRepo
 import io.mockk.MockKAnnotations
@@ -9,9 +10,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class RequestsServiceControllerTest {
     private lateinit var requestsServiceController: RequestsServiceController
+    private lateinit var requestsServiceControllerWith3PerPage: RequestsServiceController
 
     @MockK
     private lateinit var requestsRepo: RequestsRepo
@@ -27,6 +30,7 @@ class RequestsServiceControllerTest {
 
     @MockK
     private lateinit var ohGodAnotherRequest: Request
+
     @MockK
     private lateinit var ohGodYetAnotherRequest: Request
     private val amountPerPage = 2
@@ -35,6 +39,7 @@ class RequestsServiceControllerTest {
     fun setUp() {
         MockKAnnotations.init(this)
         requestsServiceController = RequestsServiceController(requestsRepo, amountPerPage)
+        requestsServiceControllerWith3PerPage = RequestsServiceController(requestsRepo, 3)
         every { requestsRepo.findAll() } returns listOf(
             request,
             anotherRequest,
@@ -60,5 +65,24 @@ class RequestsServiceControllerTest {
 
         assertEquals(1, requestDto.requests.size)
         assertNull(requestDto.nextPage)
+    }
+
+    @Test
+    fun `getRequestsHistoricalData with 3 pages per page, should return 2 pages one error if out of range`() {
+        var requestDto = requestsServiceControllerWith3PerPage.getRequestsHistoricalData(1)
+
+        assertEquals(3, requestDto.requests.size)
+        assertEquals("/requests?page=2", requestDto.nextPage)
+
+        requestDto = requestsServiceControllerWith3PerPage.getRequestsHistoricalData(2)
+
+        assertEquals(2, requestDto.requests.size)
+        assertNull(requestDto.nextPage)
+
+        assertThrows<CustomException.BadRequest.PageDoesNotExistException> {
+            requestsServiceControllerWith3PerPage.getRequestsHistoricalData(
+                3
+            )
+        }
     }
 }
